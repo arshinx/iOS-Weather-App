e17b6e9314dac3bce1c6cbb8a862b0a0af1ff359
arshin <arshinx@users.noreply.github.com>
2018-09-27 23:25:16 -0700

    Get Data

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
         let base = URL(string: "https://api.darksky.net/forecast/\(darkSkyApiKey)/")
         let forecastURL = URL(string: "37.8267,-122.4233", relativeTo: base)
         
+        // Get Data
+        let weatherData = try! Data(contentsOf: forecastURL!)
+        print(weatherData)
+        
         let currentWeather = CurrentWeather(temperature: 85.0, humidity: 0.8, precipProbability: 0.1, summary: "Hot!", icon: "clear-day")
         let viewModel = CurrentWeatherViewModel(model: currentWeather)
         displayWeather(using: viewModel)
