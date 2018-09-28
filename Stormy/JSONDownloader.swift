4b80fbe606fdc8d5f2dcd184cbf39cde3d26cebe
arshin <arshinx@users.noreply.github.com>
2018-09-28 13:50:42 -0700

    convenience init

--- Stormy/JSONDownloader.swift
+++ Stormy/JSONDownloader.swift
@@ -15,4 +15,9 @@ class JSONDownloader {
     init(configuration: URLSessionConfiguration) {
         self.session = URLSession(configuration: configuration)
     }
+    
+    convenience init() {
+        self.init(configuration: .default)
+    }
+    
 }
