2cc934ac27aae93779d3ef9d169051da11ac6974
arshin <arshinx@users.noreply.github.com>
2018-09-28 00:11:46 -0700

    resume datatask

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController {
             let json = try! JSONSerialization.jsonObject(with: data!, options: [])
             print(json)
         }
+        dataTask.resume()
         
         let currentWeather = CurrentWeather(temperature: 85.0, humidity: 0.8, precipProbability: 0.1, summary: "Hot!", icon: "clear-day")
         let viewModel = CurrentWeatherViewModel(model: currentWeather)
