7169d097db18ddd4a223a24bc4f1fee08858d84e
arshin <arshinx@users.noreply.github.com>
2018-09-29 10:52:25 -0700

    Remove Key & Failable init

--- Stormy/CurrentWeather.swift
+++ Stormy/CurrentWeather.swift
@@ -35,34 +35,3 @@ extension CurrentWeather {
         }
     }
 }
-
-// Don't override the default initializer
-extension CurrentWeather {
-    
-    struct Key {
-        static let temperature = "temperature"
-        static let humidity = "humidity"
-        static let precipitationProbability = "precipProbability"
-        static let summary = "summary"
-        static let icon = "icon"
-    }
-    
-    init?(json: [String: AnyObject]) {
-        
-        guard let tempValue = json[Key.temperature] as? Double,
-            let humidityValue = json[Key.humidity] as? Double,
-            let precipitationProbabilityValue = json[Key.precipitationProbability] as? Double,
-            let summaryStringValue = json[Key.summary] as? String,
-            let iconStringValue = json[Key.icon] as? String
-            else {
-                return nil
-        }
-        
-        self.temperature = tempValue
-        self.humidity = humidityValue
-        self.precipProbability = precipitationProbabilityValue
-        self.summary = summaryStringValue
-        self.icon = iconStringValue
-        
-    }
-}
