55cd823726f3b1ceb9563988c2fd994c372a6469
arshin <arshinx@users.noreply.github.com>
2018-09-29 10:53:36 -0700

    remove extension

--- Stormy/Weather.swift
+++ Stormy/Weather.swift
@@ -11,15 +11,3 @@ import Foundation
 struct Weather: Codable {
     let currently: CurrentWeather
 }
-
-extension Weather {
-    
-    init?(json: [String: AnyObject]) {
-        
-        guard let currentWeatherJson = json["currently"] as? [String: AnyObject], let currentWeather = CurrentWeather(json: currentWeatherJson) else {
-            return nil
-        }
-        
-        self.currently = currentWeather
-    }
-}
