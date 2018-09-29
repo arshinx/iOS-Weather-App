70be84d426fb2bd313504cac9d0d445aee8e2618
arshin <arshinx@users.noreply.github.com>
2018-09-29 10:49:19 -0700

    simplify

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -22,25 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let base = URL(string: "https://api.darksky.net/forecast/\(darkSkyApiKey)/")
-        guard let forecastURL = URL(string: "37.8267,-122.4233", relativeTo: base) else {
-            return
         }
-        
-        // Get Data Async
-        let request = URLRequest(url: forecastURL)
-        let session = URLSession(configuration: .default)
-        let dataTask = session.dataTask(with: request) { (data, response, error) in
-            // Body of Closure
-            let json = try! JSONSerialization.jsonObject(with: data!, options: [])
-            print(json)
-        }
-        dataTask.resume()
-        
-        
-        let currentWeather = CurrentWeather(temperature: 85.0, humidity: 0.8, precipProbability: 0.1, summary: "Hot!", icon: "clear-day")
-        let viewModel = CurrentWeatherViewModel(model: currentWeather)
-        displayWeather(using: viewModel)
     }
     
     func displayWeather(using viewModel: CurrentWeatherViewModel) {
