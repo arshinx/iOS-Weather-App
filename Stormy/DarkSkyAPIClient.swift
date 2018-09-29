393f86cab63d74b3b3c97da7679956fa375929da
arshin <arshinx@users.noreply.github.com>
2018-09-29 11:07:00 -0700

    Async on main thread

--- Stormy/DarkSkyAPIClient.swift
+++ Stormy/DarkSkyAPIClient.swift
@@ -43,27 +43,30 @@ class DarkSkyAPIClient {
         
         let task = session.dataTask(with: request) { (data, res, error) in
             
-            if let data = data {
+            DispatchQueue.main.async {
                 
-                guard let httpResponse = res as? HTTPURLResponse else {
-                    completion(nil, DarkSkyError.requestFailed)
-                    return
-                }
-                
-                // Check if request is successful
-                if httpResponse.statusCode == 200 {
-                    do {
-                        let weather = try self.decoder.decode(Weather.self, from: data)
-                        completion(weather, nil)
-                    } catch let error {
-                        completion(nil, error)
+                if let data = data {
+                    
+                    guard let httpResponse = res as? HTTPURLResponse else {
+                        completion(nil, DarkSkyError.requestFailed)
+                        return
                     }
-                } else { // status not "success/200"
-                    completion(nil, DarkSkyError.invalidData)
+                    
+                    // Check if request is successful
+                    if httpResponse.statusCode == 200 {
+                        do {
+                            let weather = try self.decoder.decode(Weather.self, from: data)
+                            completion(weather, nil)
+                        } catch let error {
+                            completion(nil, error)
+                        }
+                    } else { // status not "success/200"
+                        completion(nil, DarkSkyError.invalidData)
+                    }
+                    
+                } else if let error = error {
+                    completion(nil, error)
                 }
-                
-            } else if let error = error {
-                completion(nil, error)
             }
         }
         // Resume the task
